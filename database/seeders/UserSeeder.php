54d397990f25ee9baa82ddf98fdf51e9a3ad0692
rencon0417 <yugo04172930@icloud.com>
2023-12-06 14:24:14 +0900

    items下準備

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * データベースに対するデータ設定の実行
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('yakyu8910'),
+        ]);
+    }
+}
---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div v-if="$page.props.flash.message" class="bg-blue-300">
+        {{ $page.props.flash.message }}
+    </div>
+Inertiaインデックスです。
+</template>

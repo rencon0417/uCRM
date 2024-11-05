f6fe2584253d559c09a0387b109d983ed895ab63
rencon0417 <yugo04172930@icloud.com>
2024-11-06 01:09:46 +0900

    “edit”

--- resources/js/Components/QuestionTemp1.vue
+++ resources/js/Components/QuestionTemp1.vue
@@ -8,10 +8,10 @@
           <p>{{ content }}</p>
         </div>
       </div>
-      <div class="flex justify-end text-gray-500 text-xs">
+      <div class="flex justify-end text-gray-400 text-xs">
         {{ props.title }}
       </div>
-      <div class="flex justify-end text-gray-500 text-xs">
+      <div class="flex justify-end text-gray-400 text-xs">
         {{ props.subject }}
       </div>
     </div>

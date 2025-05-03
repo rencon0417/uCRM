64fcb025e15141f984fb50c7ee002fdac9ab91e3
rencon0417 <yugo04172930@icloud.com>
2025-05-03 17:48:07 +0900

    Auto commit: 2025-05-03 17:48:07

--- public/build/assets/Show-8d72b051.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./app-91c74f92.js";const n={__name:"Show",props:{id:String},setup(t){return(a,e)=>r(t.id)}};export{n as default};
--- resources/js/Components/QuestionTemp2.vue
+++ resources/js/Components/QuestionTemp2.vue
@@ -1,19 +1,25 @@
 <template>
     <div  v-for="(content, index) in props.contents" :key="index"
           class="bg-white px-6 py-3 border border-gray-300 rounded-lg shadow-md">
-      <h2 class="text-lg font-semibold mb-4">問題{{ index + questionNumber }}</h2>
-      <div class="grid gap-2">
-        <div class="grid gap-2 text-gray-700 select-none grid-cols-[2em_1fr]" >
-          <span class="font-semibold">{{ props.labels[index] }}：</span>
-          <p v-html="content"></p>
+        <!-- h2 -->
+        <h2 class="text-base sm:text-lg font-semibold mb-4">
+            問題{{ index + questionNumber }}
+        </h2>
+        <div class="grid gap-2">
+            <div class="grid gap-2 text-gray-700 select-none grid-cols-[2em_1fr]" >
+                <span class="font-semibold text-sm sm:text-base">
+                {{ props.labels[index] }}：
+                </span>
+                <p v-html="content" class="text-sm sm:text-base"></p>
+            </div>
         </div>
-      </div>
-      <div class="flex justify-end text-gray-400 text-xxs lg:text-xs">
-        {{ props.title }}
-      </div>
-      <div class="flex justify-end text-gray-400 text-xxs lg:text-xs">
-        {{ props.subject }}
-      </div>
+        <!-- title / subject -->
+        <div class="flex justify-end text-xs sm:text-sm text-gray-400">
+            {{ props.title }}
+        </div>
+        <div class="flex justify-end text-xs sm:text-sm text-gray-400">
+            {{ props.subject }}
+        </div>
     </div>
 </template>
 

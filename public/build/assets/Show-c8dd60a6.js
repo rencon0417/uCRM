f2313e8e8502977b5eebdd5a0ae73c7fa64ec3bc
rencon0417 <yugo04172930@icloud.com>
2025-05-03 18:43:22 +0900

    Auto commit: 2025-05-03 18:43:22

--- public/build/assets/Show-c8dd60a6.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./app-6371fd60.js";const n={__name:"Show",props:{id:String},setup(t){return(a,e)=>r(t.id)}};export{n as default};
--- resources/js/Components/QuestionTemp4.vue
+++ resources/js/Components/QuestionTemp4.vue
@@ -2,19 +2,21 @@
     <div  v-for="(content, index) in props.contents"
         :key="index"
         class="bg-white px-2 sm:px-4 md:px-6 py-3 border border-gray-300 rounded-lg shadow-md">
-        <!-- h2 -->
-        <h2 class="text-sm sm:text-base md:text-lg font-semibold mb-4">
+        <!-- 問題番号 -->
+        <h2 class="text-base sm:text-lg font-semibold mb-4">
             問題{{ index + questionNumber }}
         </h2>
-        <div class="grid gap-2">
-            <div class="grid gap-2 text-gray-700 select-none grid-cols-[2em_1fr]" >
-                <span class="font-semibold text-sm sm:text-base">
-                {{ props.labels[index] }}：
-                </span>
-                <p v-html="content" class="text-sm sm:text-base"></p>
-            </div>
+          <!-- 回答 -->
+        <div class="grid gap-2 text-gray-700 select-none grid-cols-[2em_1fr]">
+            <span class="font-semibold text-sm sm:text-base">
+            {{ props.labels[index] }}：
+            </span>
+            <p class="text-sm sm:text-base" v-html="content.answer"></p>
         </div>
-        <!-- title / subject -->
+        <!-- 解説 -->
+        <p class="text-sm sm:text-base text-gray-700 select-none" v-html="content.explanation"></p>
+
+        <!-- タイトルと科目 -->
         <div class="flex justify-end text-xs sm:text-sm text-gray-400">
             {{ props.title }}
         </div>
@@ -29,7 +31,7 @@
 
   interface Props {
     questionNumber: number
-    contents: string[]
+    contents: { answer: string; explanation: string }[]
     labels: string[]
     title: string[]
     subject: string[]

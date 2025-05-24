187388bcb14951f614e9ec3d5e4a79c61386dacc
rencon0417 <yugo04172930@icloud.com>
2025-05-24 20:58:01 +0900

    Auto commit: 2025-05-24 20:58:01

--- resources/js/Components/QuestionTemp4.vue
+++ resources/js/Components/QuestionTemp4.vue
@@ -19,7 +19,10 @@
             <p class="text-base" v-html="content.answer"></p>
         </div>
         <!-- 解説 -->
-        <div class="my-4 p-4 bg-gray-50 rounded text-sm sm:text-base leading-relaxed text-gray-800 whitespace-pre-line" v-html="content.explanation"></div>
+        <div class="my-4 p-4 bg-gradient-to-br from-gray-50 to-white border border-gray-200 rounded-md">
+        <div class="mb-2 text-base font-semibold text-purple-600">解説：</div>
+        <div class="text-sm sm:text-base leading-relaxed text-gray-800 whitespace-pre-line" v-html="content.explanation"></div>
+        </div>
 
         <!-- タイトルと科目 -->
         <div class="flex justify-end text-gray-400 text-xxs lg:text-xs">

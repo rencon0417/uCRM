c913f30723e675ef860ece7ddfb178080e48da8b
rencon0417 <yugo04172930@icloud.com>
2025-05-24 16:26:56 +0900

    Auto commit: 2025-05-24 16:26:56

--- resources/js/Components/QuestionTemp4.vue
+++ resources/js/Components/QuestionTemp4.vue
@@ -18,8 +18,11 @@
             </span>
             <p class="text-sm sm:text-base" v-html="content.answer"></p>
         </div>
-        <!-- 解説 -->
-        <p class="text-sm sm:text-base text-gray-700 select-none" v-html="content.explanation"></p>
+<!-- 解説 -->
+<div class="my-4 p-4 bg-gray-50 rounded-md border border-gray-200 space-y-2 text-base">
+  <p class="font-semibold text-gray-600">【解説】</p>
+  <div v-html="content.explanation" class="text-gray-700 leading-relaxed"></div>
+</div>
 
         <!-- タイトルと科目 -->
         <div class="flex justify-end text-gray-400 text-xxs lg:text-xs">

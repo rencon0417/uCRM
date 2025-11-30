145e0233327e58f408aaedda3544fe49dadc61a7
rencon0417 <yugo04172930@icloud.com>
2025-11-30 15:14:16 +0900

    2025-11-30 の更新

---
+++ public/build/assets/Index-8e91b3e0.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{f as s,g as a,t as r,z as o,m as n,F as c}from"./app-cd51e310.js";const m={},p={key:0,class:"bg-blue-300"};function l(e,f){return s(),a(c,null,[e.$page.props.flash.message?(s(),a("div",p,r(e.$page.props.flash.message),1)):o("",!0),n(" Inertiaインデックスです。 ")],64)}const d=t(m,[["render",l]]);export{d as default};
